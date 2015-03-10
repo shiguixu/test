733355d2656d1f8b776a39bb01daa34962bdc9a9
shiguixu <453655321@qq.com>
2015-03-10 17:23:26 +0800

    第一次提交

---
+++ nisb/src/test1.java
@@ -0,0 +1,26 @@
+/*
+ *www.dyr.com
+ *Copyright(c)2014 All Rights Reserved.
+ */
+/**
+ * 
+ */
+
+/**
+ * Project:nisb
+ * package:
+ * FileName:test1.java
+ * Comments:
+ * JDK  Version:
+ * Author:Casper
+ * Create Date:2015-3-10 обнГ5:11:39
+ * Modified By:Casper
+ * Modified Time:
+ * What is Modified:
+ * Version:
+ */
+public class test1 {
+	public static void main(String[] args) {
+		System.out.println("nishisb");
+	}
+}
